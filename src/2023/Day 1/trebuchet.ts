53b5b1e73e4f8a3705c8b8b9950e0630b269a908
Ábel Nagy <abel.nagy@semilab.hu>
2023-12-01 14:01:59 +0100

    Solved Day 1 Problem 1

---
+++ src/2023/Day 1/trebuchet.ts
@@ -0,0 +1,29 @@
+import * as fs from "fs";
+
+export class Trebuchet {
+	exampleInput = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
+	inputPath = "src/2023/Day 1/input.txt";
+
+	run(input: string) {
+		const lines = input.split("\n");
+		const numbers = lines.map((line) =>
+			line
+				.split("")
+				.filter((char) => char >= "0" && char <= "9")
+				.map((char) => parseInt(char))
+		);
+
+		const sum = numbers
+			.map((number) => {
+				return [number[0], number[number.length - 1]];
+			})
+			.map((number) => parseInt(number.join("")))
+			.reduce((a, b) => a + b);
+
+		console.log(sum);
+	}
+
+	importInputFile() {
+		return fs.readFileSync(this.inputPath, "utf-8");
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Trebuchet } from "./2023/Day 1/trebuchet";
+
+const trebuchet = new Trebuchet();
+trebuchet.run(trebuchet.exampleInput);
+trebuchet.run(trebuchet.importInputFile());
